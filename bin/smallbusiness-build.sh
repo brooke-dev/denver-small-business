749c43c61d28daa5007abd8496d22746c1676156
Brooke Ross <brooke.ross2800@gmail.com>
2022-12-07 15:14:47 -0700

    added to build file

--- bin/smallbusiness-build.sh
+++ bin/smallbusiness-build.sh
@@ -5,5 +5,11 @@ set -o errexit
 bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
+
+# Add build commands for front end
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
 bundle exec rake db:migrate 
 bundle exec rake db:seed
